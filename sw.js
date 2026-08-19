1480d5e0f20fa8fa9557e3101e53ca2b0a9b137b
Amitoje Singh Bakshi <amitojeSBakshi@users.noreply.github.com>
2026-08-19 23:46:34 +0530

    Bump refresh stamp to 19 Aug; drop stale swept-stamp test

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-﻿/* Mumbai Circle - build 20260819-175136-1093 */
-var CACHE = "mumbai-circle-20260819-175136-1093";
+﻿/* Mumbai Circle - build 20260819-181523-136 */
+var CACHE = "mumbai-circle-20260819-181523-136";
 var ASSETS = ["./", "./index.html", "./manifest.json",
               "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
