063d0550a07c6b5e2ff1bb2278fbeed96ff7dfca
Amitoje Singh Bakshi <amitojeSBakshi@users.noreply.github.com>
2026-09-11 23:38:34 +0530

    Refresh: 119 new events, verification fixes, expired cleanup

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-﻿/* Mumbai Circle - build 20260909-193330-5675 */
-var CACHE = "mumbai-circle-20260909-193330-5675";
+﻿/* Mumbai Circle - build 20260911-180416-1067 */
+var CACHE = "mumbai-circle-20260911-180416-1067";
 var ASSETS = ["./", "./index.html", "./manifest.json",
               "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
